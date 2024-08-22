6931fad5cb89e617991f216ec0d9c5b6287f6b6b
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-08-22 17:20:23 +0600

    Update DetailsByIDService.js

--- inventory-mern-backend/src/services/common/DetailsByIDService.js
+++ inventory-mern-backend/src/services/common/DetailsByIDService.js
@@ -1,23 +1,23 @@
 const mongoose = require("mongoose");
-const DetailsByIDService= async (Request,DataModel) => {
-    try{
+const DetailsByIDService = async (Request, DataModel) => {
+    try {
 
-        let DetailsID=Request.params.id;
-        let UserEmail=Request.headers['email'];
+        let DetailsID = Request.params.id;
+        let UserEmail = Request.headers['email'];
 
         const ObjectId = mongoose.Types.ObjectId;
-        let QueryObject={};
-        QueryObject['_id']=ObjectId(DetailsID);
-        QueryObject['UserEmail']=UserEmail;
+        let QueryObject = {};
+        QueryObject['_id'] = ObjectId(DetailsID);
+        QueryObject['UserEmail'] = UserEmail;
 
 
         let data = await DataModel.aggregate([
-            {$match: QueryObject}
+            { $match: QueryObject }
         ])
-        return {status: "success", data: data}
+        return { status: "success", data: data }
     }
     catch (error) {
-        return {status: "fail", data: error}
+        return { status: "fail", data: error }
     }
 }
-module.exports=DetailsByIDService
+module.exports = DetailsByIDService
