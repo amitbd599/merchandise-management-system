7542480e6360d7527e15b235a6b6548ca48d712c
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-08-22 17:12:00 +0600

    Update DropDownService.js

--- inventory-mern-backend/src/services/common/DropDownService.js
+++ inventory-mern-backend/src/services/common/DropDownService.js
@@ -1,14 +1,14 @@
-const DropDownService= async (Request,DataModel,Projection) => {
-    try{
-        let UserEmail=Request.headers['email'];
+const DropDownService = async (Request, DataModel, Projection) => {
+    try {
+        let UserEmail = Request.headers['email'];
         let data = await DataModel.aggregate([
-            {$match: {UserEmail:UserEmail}},
-            {$project:Projection}
+            { $match: { UserEmail: UserEmail } },
+            { $project: Projection }
         ])
-        return {status: "success", data: data}
+        return { status: "success", data: data }
     }
     catch (error) {
-        return {status: "fail", data: error}
+        return { status: "fail", data: error }
     }
 }
-module.exports=DropDownService
+module.exports = DropDownService
