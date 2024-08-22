dcf46666065109c990c69002a6abb28d776adb66
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-08-22 17:19:28 +0600

    Update api.js

--- inventory-mern-backend/src/routes/api.js
+++ inventory-mern-backend/src/routes/api.js
@@ -32,7 +32,7 @@ router.post("/CreateBrand", AuthVerifyMiddleware, BrandsController.CreateBrand);
 router.post("/UpdateBrand/:id", AuthVerifyMiddleware, BrandsController.UpdateBrand);
 router.get("/BrandList/:pageNo/:perPage/:searchKeyword", AuthVerifyMiddleware, BrandsController.BrandList);
 router.get("/BrandDropDown", AuthVerifyMiddleware, BrandsController.BrandDropDown);
-router.get("/DeleteBrand/:id", AuthVerifyMiddleware, BrandsController.DeleteBrand);
+router.delete("/DeleteBrand/:id", AuthVerifyMiddleware, BrandsController.DeleteBrand);
 router.get("/BrandDetailsByID/:id", AuthVerifyMiddleware, BrandsController.BrandDetailsByID);
 
 
