11ab3d1208aca58a927ea3c8fdec217a3b02ba53
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-08-22 17:29:53 +0600

    Update ListService.js

--- inventory-mern-backend/src/services/common/ListService.js
+++ inventory-mern-backend/src/services/common/ListService.js
@@ -1,44 +1,44 @@
-const ListService= async (Request,DataModel,SearchArray) => {
-    try{
+const ListService = async (Request, DataModel, SearchArray) => {
+    try {
 
         let pageNo = Number(Request.params.pageNo);
         let perPage = Number(Request.params.perPage);
         let searchValue = Request.params.searchKeyword;
-        let UserEmail=Request.headers['email'];
+        let UserEmail = Request.headers['email'];
 
         let skipRow = (pageNo - 1) * perPage;
 
         let data;
 
-        if (searchValue!=="0") {
-            let SearchQuery = {$or:SearchArray}
+        if (searchValue !== "0") {
+            let SearchQuery = { $or: SearchArray }
             data = await DataModel.aggregate([
-                    {$match: {UserEmail:UserEmail}},
-                    {$match: SearchQuery},
-                    {
-                    $facet:{
-                        Total:[{$count: "count"}],
-                        Rows:[{$skip: skipRow}, {$limit: perPage}],
+                { $match: { UserEmail: UserEmail } },
+                { $match: SearchQuery },
+                {
+                    $facet: {
+                        Total: [{ $count: "count" }],
+                        Rows: [{ $skip: skipRow }, { $limit: perPage }],
                     }
                 }
             ])
         }
         else {
             data = await DataModel.aggregate([
-                {$match: {UserEmail:UserEmail}},
+                { $match: { UserEmail: UserEmail } },
                 {
-                    $facet:{
-                        Total:[{$count: "count"}],
-                        Rows:[{$skip: skipRow}, {$limit: perPage}],
+                    $facet: {
+                        Total: [{ $count: "count" }],
+                        Rows: [{ $skip: skipRow }, { $limit: perPage }],
                     }
                 }
             ])
 
         }
-        return {status: "success", data: data}
+        return { status: "success", data: data }
     }
     catch (error) {
-        return {status: "fail", data: error}
+        return { status: "fail", data: error }
     }
 }
-module.exports=ListService
+module.exports = ListService
