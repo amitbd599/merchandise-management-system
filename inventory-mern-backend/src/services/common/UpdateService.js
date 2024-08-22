34d6beb67c8493127fa52576703eae3a6688ff8e
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-08-22 17:10:18 +0600

    Update UpdateService.js

--- inventory-mern-backend/src/services/common/UpdateService.js
+++ inventory-mern-backend/src/services/common/UpdateService.js
@@ -1,14 +1,14 @@
-const UpdateService= async (Request,DataModel) => {
-    try{
-        let UserEmail=Request.headers['email'];
-        let id=Request.params.id;
-        let PostBody=Request.body;
-        let data = await DataModel.updateOne({_id:id,UserEmail:UserEmail},PostBody);
-        return {status: "success", data: data}
+const UpdateService = async (Request, DataModel) => {
+    try {
+        let UserEmail = Request.headers['email'];
+        let id = Request.params.id;
+        let PostBody = Request.body;
+        let data = await DataModel.updateOne({ _id: id, UserEmail: UserEmail }, PostBody);
+        return { status: "success", data: data }
     }
     catch (error) {
-        return {status: "fail", data: error}
+        return { status: "fail", data: error.toString() }
     }
 }
-module.exports=UpdateService
+module.exports = UpdateService
 
