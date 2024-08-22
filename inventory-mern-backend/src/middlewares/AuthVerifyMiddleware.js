3c0b5fd918d640aacdd7f49b77e5dbc64c8a8223
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-08-22 13:20:29 +0600

    login auth ready

--- inventory-mern-backend/src/middlewares/AuthVerifyMiddleware.js
+++ inventory-mern-backend/src/middlewares/AuthVerifyMiddleware.js
@@ -1,15 +1,14 @@
 var jwt = require('jsonwebtoken');
-module.exports=(req,res,next)=>{
-    let Token=req.headers['token'];
-    jwt.verify(Token,"SecretKey123456789",function (err,decoded) {
-        if(err){
-            console.log(Token)
-            res.status(401).json({status:"unauthorized"})
+module.exports = (req, res, next) => {
+    let Token = req.headers['token'];
+    jwt.verify(Token, "SecretKey123456789", function (err, decoded) {
+        if (err) {
+
+            res.status(401).json({ status: "unauthorized" })
         }
         else {
-            let email=decoded['data'];
-            console.log(email)
-            req.headers.email=email
+            let email = decoded['data'];
+            req.headers.email = email
             next();
         }
     })
