8129bcc6f17fdda0913e1b77d3f51c05836c9c89
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-08-23 14:13:02 +0600

    Update DetailsByIDService.js

--- inventory-mern-backend/src/services/common/DetailsByIDService.js
+++ inventory-mern-backend/src/services/common/DetailsByIDService.js
@@ -12,11 +12,7 @@ const DetailsByIDService = async (Request, DataModel) => {
 
 
         let data = await DataModel.aggregate([
-
-
             { $match: QueryObject }
-
-
         ])
         return { status: "success", data: data }
     }
