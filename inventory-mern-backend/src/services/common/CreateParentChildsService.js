33255ef75f2dc2bdcf50824ef865354f62037f84
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-08-24 12:30:30 +0600

    CreatePurchases api add

--- inventory-mern-backend/src/services/common/CreateParentChildsService.js
+++ inventory-mern-backend/src/services/common/CreateParentChildsService.js
@@ -1,29 +1,29 @@
 const mongoose = require("mongoose");
 
-const CreateParentChildsService= async (Request, ParentModel, ChildsModel,JoinPropertyName) => {
+const CreateParentChildsService = async (Request, ParentModel, ChildsModel, JoinPropertyName) => {
 
     // Create Transaction Session
     const session = await mongoose.startSession();
 
-    try{
+    try {
 
         // Begin Transaction
         await session.startTransaction();
 
         // First Database Process
-        let Parent=Request.body['Parent'];
-        Parent.UserEmail=Request.headers['email'];
-        let ParentCreation = await ParentModel.create([Parent],{ session });
+        let Parent = Request.body['Parent'];
+        Parent.UserEmail = Request.headers['email'];
+        let ParentCreation = await ParentModel.create([Parent], { session });
 
 
         // Second Database Process
-        let Childs=Request.body['Childs'];
+        let Childs = Request.body['Childs'];
         await Childs.forEach((element) => {
-            element[JoinPropertyName] =ParentCreation[0]['_id'];
-         element['UserEmail']=Request.headers['email'];
+            element[JoinPropertyName] = ParentCreation[0]['_id'];
+            element['UserEmail'] = Request.headers['email'];
         });
 
-        let ChildsCreation = await ChildsModel.insertMany(Childs,{ session });
+        let ChildsCreation = await ChildsModel.insertMany(Childs, { session });
 
 
 
@@ -31,7 +31,7 @@ const CreateParentChildsService= async (Request, ParentModel, ChildsModel,JoinPr
         await session.commitTransaction();
         session.endSession();
 
-        return {status: "success", Parent: ParentCreation,Childs:ChildsCreation}
+        return { status: "success", Parent: ParentCreation, Childs: ChildsCreation }
 
 
     }
@@ -39,7 +39,7 @@ const CreateParentChildsService= async (Request, ParentModel, ChildsModel,JoinPr
         // Roll Back Transaction if Fail
         await session.abortTransaction();
         session.endSession();
-        return {status: "fail", data: error}
+        return { status: "fail", data: error }
     }
 }
-module.exports=CreateParentChildsService
+module.exports = CreateParentChildsService
