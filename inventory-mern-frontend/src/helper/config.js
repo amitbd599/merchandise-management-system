c9d2855bbaf5340727425fffa52fd82947902a06
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-09-11 00:56:53 +0600

    add update

--- inventory-mern-frontend/src/helper/config.js
+++ inventory-mern-frontend/src/helper/config.js
@@ -1,2 +1,2 @@
-// export const BaseURL = "https://merchandise-api.amitjs.com/api/v1";
-export const BaseURL = "http://localhost:8000/api/v1"; 
+export const BaseURL = "https://merchandise-api.amitjs.com/api/v1";
+// export const BaseURL = "http://localhost:8000/api/v1"; 
