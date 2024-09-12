ca3fb050fef2858e9d73a2429b780b955069dcbd
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-09-12 19:00:55 +0600

    Update ExpensesController.js

--- inventory-mern-backend/src/controllers/Expenses/ExpensesController.js
+++ inventory-mern-backend/src/controllers/Expenses/ExpensesController.js
@@ -5,32 +5,33 @@ const ListOneJoinService = require("../../services/common/ListOneJoinService");
 const DeleteService = require("../../services/common/DeleteService");
 const DetailsByIDService = require("../../services/common/DetailsByIDService");
 
-exports.CreateExpenses=async (req, res) => {
-    let Result= await CreateService(req,DataModel);
+exports.CreateExpenses = async (req, res) => {
+    let Result = await CreateService(req, DataModel);
     res.status(200).json(Result)
 }
 
-exports.UpdateExpenses=async (req, res) => {
-    let Result=await UpdateService(req,DataModel)
+exports.UpdateExpenses = async (req, res) => {
+    let Result = await UpdateService(req, DataModel)
     res.status(200).json(Result)
 }
 
-exports.ExpensesList=async (req, res) => {
-    let SearchRgx = {"$regex": req.params.searchKeyword, "$options": "i"}
-    let SearchArray=[{Note: SearchRgx},{'Type.Name': SearchRgx}]
-    let JoinStage= {$lookup: {from: "expensetypes", localField: "TypeID", foreignField: "_id", as: "Type"}}
-    let Result=await ListOneJoinService(req,DataModel,SearchArray,JoinStage);
+exports.ExpensesList = async (req, res) => {
+    let SearchRgx = { "$regex": req.params.searchKeyword, "$options": "i" }
+    let SearchArray = [{ Note: SearchRgx }, { 'Type.Name': SearchRgx }]
+    let JoinStage = { $lookup: { from: "expensetypes", localField: "TypeID", foreignField: "_id", as: "Type" } }
+
+    let Result = await ListOneJoinService(req, DataModel, SearchArray, JoinStage, JoinStage);
     res.status(200).json(Result)
 }
 
-exports.ExpenseDetailsByID=async (req, res) => {
-    let Result= await DetailsByIDService(req,DataModel)
+exports.ExpenseDetailsByID = async (req, res) => {
+    let Result = await DetailsByIDService(req, DataModel)
     res.status(200).json(Result)
 }
 
 
-exports.DeleteExpense=async (req, res) => {
-    let Result=await DeleteService(req,DataModel);
+exports.DeleteExpense = async (req, res) => {
+    let Result = await DeleteService(req, DataModel);
     res.status(200).json(Result)
 }
 
